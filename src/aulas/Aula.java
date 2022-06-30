e7f0d4cd7ffa383a188462db4c4181e69f9bd017
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-06-30 09:20:23 -0300

    Att 29/06

--- src/aulas/Aula.java
+++ src/aulas/Aula.java
@@ -2,12 +2,6 @@ package aulas;
 
 public class Aula {
 	public static void main(String[] args) {
-		Caneta c1 = new Caneta();
-		c1.modelo = "BIC cristal";
-		c1.cor = "Azul";
-		c1.carga = 80;
-		c1.tampada = true;
-		c1.status();
-		c1.rabiscar();
+		
 	}
 }
